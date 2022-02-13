b4d12763509463d1764023427ddc42b035c09248
KyeongTaek <tommyholic@naver.com>
2022-02-14 06:03:26 +0900

    Update board

--- src/main/java/com/cbnu/zmz/entity/Board.java
+++ src/main/java/com/cbnu/zmz/entity/Board.java
@@ -3,9 +3,12 @@ package com.cbnu.zmz.entity;
 import lombok.*;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+
+
 
 @Entity
-@Table(name="post_id")
+@Table(name="Board")
 @ToString
 @Getter
 @Builder
@@ -13,9 +16,32 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class Board {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     Long post_id;
 
     @Column(length = 2000, nullable = false)
     String post_content;
+
+    @Column(length = 20, nullable = true)
+    String post_title;
+
+    @Column(nullable = false)
+    Boolean is_secret;
+
+    @Column(nullable = false)
+    LocalDateTime post_time;
+
+    @Column(nullable = true)
+    Long post_comment;
+
+    @Column(nullable = true)
+    Long post_like;
+
+    @Column(nullable = true)
+    Long post_view;
+
+    @ManyToOne//게시글은 오직 하나의 사용자가 있다
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    User user;
+
 }
--- src/test/java/com/cbnu/zmz/repository/BoardRepositoryTests.java
+++ src/test/java/com/cbnu/zmz/repository/BoardRepositoryTests.java
@@ -1,5 +1,6 @@
 package com.cbnu.zmz.repository;
 
+import com.cbnu.zmz.entity.Board;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,9 +11,15 @@ import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Commit;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.IntStream;
+import java.util.Date;
+import java.sql.Timestamp;
+import java.time.LocalTime.*;
+
+import static java.time.LocalDateTime.*;
 
 @SpringBootTest
 public class BoardRepositoryTests {
@@ -26,4 +33,16 @@ public class BoardRepositoryTests {
 //        결과 $Proxy108  왜냐면 해당 객체에 클래스 이름이 없기 때문에 $가 붙어서 나옴
     }
 
+    @Test
+    public void testInsertDummy(){
+        IntStream.rangeClosed(1, 100).forEach(i -> {
+            Board board = Board.builder()
+                    .post_id((long)i)
+                    .is_secret(Boolean.TRUE)
+                    .post_content("Content..." + i)
+                    .post_time(now())
+                    .build();
+            boardRepository.save(board);
+        });
+    }
 }
